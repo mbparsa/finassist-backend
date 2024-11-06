78bcc30809f76c3beaa42dd084732a411bfb6235
MB Parsa <mbparsa@mac.lan>
2024-11-05 23:31:07 -0800

    removing db credentials and replacing with env variable

--- database/db.go
+++ database/db.go
@@ -6,14 +6,17 @@ import (
     "gorm.io/driver/postgres"
     "gorm.io/gorm"
 	"github.com/mbparsa/finassist-backend/models"
+	"os"
 )
 
 var DB *gorm.DB
 
 func ConnectDB() {
     var err error
-    dsn := "host=localhost user=postgres password=<>! dbname=finassist port=5432 sslmode=disable TimeZone=America/Los_Angeles"
-    DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dbUsername := os.Getenv("DB_USERNAME")
+    dbPassword := os.Getenv("DB_PASSWORD")
+    dsn := fmt.Sprintf("host=localhost user=%s password=%s dbname=finassist port=5432 sslmode=disable TimeZone=America/Los_Angeles", dbUsername, dbPassword)
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
     if err != nil {
         log.Fatalf("Error connecting to the database: %v", err)
     }
